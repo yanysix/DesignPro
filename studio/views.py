5c79a6d482936b7b300ab61b95dcfc1c8c0170cc
Федотова Алевтина Семеновна <gr431_fease@ttit.local>
2024-11-28 11:51:28 +0700

    fifth commit

--- studio/views.py
+++ studio/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import login
 
 def index(request):
 
@@ -13,3 +15,14 @@ def sign_up(request):
     if request.method == 'GET':
         form = RegisterForm()
         return render(request, 'studio/register.html', { 'form': form})
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'Вы успешно зарегестрировались')
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'studio/register.html', {'form': form})
